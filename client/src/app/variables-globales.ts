37ea119e21fbef8d258d36b919ca14a944598b92
Florian VACHER <vacherflorian43@gmail.com>
2023-02-09 23:54:50 +0100

    Update back link

--- client/src/app/variables-globales.ts
+++ client/src/app/variables-globales.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class VariablesGlobales {
-    urlBack: string = 'http://localhost:5001/api/';
+    urlBack: string = 'https://gen-my-app.vercel.app/api/';
     token = sessionStorage.getItem('token');
 }
