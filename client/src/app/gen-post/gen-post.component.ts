24ecbf72a55aac943d66a09135b9c38cec59a723
Florian VACHER <vacherflorian43@gmail.com>
2023-02-10 02:33:02 +0100

    Add loading GIF

--- client/src/app/gen-post/gen-post.component.ts
+++ client/src/app/gen-post/gen-post.component.ts
@@ -16,6 +16,9 @@ export class GenPostComponent {
   multiplePost: boolean = false;
   historyVisible: boolean = false;
   allPost: Post[] = [];
+  postGenerating: boolean = false;
+  noPostMessage: boolean = true;
+  postGenIndex: number = 1;
   historyPost: HistoryPost[] = [];
 
   constructor(private vGlobales: VariablesGlobales, private http: HttpClient, private router: Router, private clipboardService: ClipboardService) {
@@ -37,14 +40,14 @@ export class GenPostComponent {
         const body = {};
         this.historyPost = [];
         this.http.post<any>(this.vGlobales.urlBack + 'posts', body, { headers }).subscribe(data => {
-          for(let i = 0; i < data.length; i++) {
+          for (let i = 0; i < data.length; i++) {
             this.historyPost.push({
               id: i + 1,
               text: data[i].text,
               keyWords: data[i].keywords,
-              date: data[i].createdAt 
+              date: data[i].createdAt
             });
-          } 
+          }
         });
         break;
     }
@@ -60,35 +63,42 @@ export class GenPostComponent {
   }
 
   onSubmit(f: NgForm) {
-    console.log(f.value);
-    console.log(f.valid);
     this.error = "";
     if (!f.valid) {
       this.error = "Le formulaire n'est pas valide ! Des champs requis sont manquants !";
     } else {
-      let index = 1;
-      let nbPost = this.multiplePost ? f.value.numberPost : 1;
-      let keyWords = `#${f.value.socialMedia}#${f.value.thematic}#${f.value.objective}`;
-      f.value.keyWords.split(',').forEach((keyWord: string) => {
-        keyWords += `#${keyWord}`;
-      })
-      const headers = new HttpHeaders().set('Authorization', 'Bearer ' + sessionStorage.getItem('token'));
-      const body = {
-        prompt: `Génère moi une publication pour ${f.value.socialMedia} sur la thématique ${f.value.thematic} dans le but de ${f.value.objective} incluant les mots-clés suivants : ${f.value.keyWords}`,
-        keywords: keyWords
-      };
       this.allPost = [];
-      for (let i = 1; i <= nbPost; i++) {         
-        this.http.post<any>(this.vGlobales.urlBack + 'openai/generate/post', body, { headers }).subscribe(data => {
-          this.allPost.push({
-            id: index,
-            text: data.answer,
-            keyWords: keyWords
-          });
-          index++;
-        });
-      }
+      this.noPostMessage = false;
+      this.postGenerating = true;
+      this.generatePost(f);
     }
+  }
+
+  generatePost(f: NgForm) {
+    let nbPost = this.multiplePost ? f.value.numberPost : 1;
+    let keyWords = `#${f.value.socialMedia}#${f.value.thematic}#${f.value.objective}`;
+    f.value.keyWords.split(',').forEach((keyWord: string) => {
+      keyWords += `#${keyWord}`;
+    })
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + sessionStorage.getItem('token'));
+    const body = {
+      prompt: `Génère moi une publication pour ${f.value.socialMedia} sur la thématique ${f.value.thematic} dans le but de ${f.value.objective} incluant les mots-clés suivants : ${f.value.keyWords}`,
+      keywords: keyWords
+    };
+    this.http.post<any>(this.vGlobales.urlBack + 'openai/generate/post', body, { headers }).subscribe(data => {
+      this.allPost.push({
+        id: this.postGenIndex,
+        text: data.answer,
+        keyWords: keyWords
+      });
+      if (this.postGenIndex == nbPost) {
+        this.postGenerating = false;
+        this.postGenIndex = 1;
+      } else {
+        this.generatePost(f);
+      }
+      this.postGenIndex++;
+    });
   }
 
   copyContent(id: string) {
